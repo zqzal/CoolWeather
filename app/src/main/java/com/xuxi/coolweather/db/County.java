e87a3585ddaba56843b9e19dd69945528767fc62
zqzal <936952020@qq.com>
2019-08-26 16:30:00 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/xuxi/coolweather/db/County.java
@@ -0,0 +1,36 @@
+package com.xuxi.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
