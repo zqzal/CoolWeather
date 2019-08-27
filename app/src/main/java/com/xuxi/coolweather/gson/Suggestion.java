4ea3b8d04652a066f61b99662c1502b12d75547b
zqzal <936952020@qq.com>
2019-08-27 18:15:00 +0800

    gson 类

---
+++ app/src/main/java/com/xuxi/coolweather/gson/Suggestion.java
@@ -0,0 +1,31 @@
+package com.xuxi.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Suggestion {
+
+    @SerializedName("comf")
+    public Comfort comfort;
+
+    @SerializedName("cw")
+    public Carwash carwash;
+
+
+    public Sport sport;
+
+    public class Comfort{
+        @SerializedName("txt")
+        public String info;
+    }
+
+    public class Carwash{
+        @SerializedName("txt")
+        public String info;
+    }
+
+    public class Sport{
+        @SerializedName("txt")
+        public String info;
+    }
+
+}
