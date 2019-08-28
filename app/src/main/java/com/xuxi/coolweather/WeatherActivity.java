124288192c0bcbaa0dff3fc44eaf722f9a2856ca
zqzal <936952020@qq.com>
2019-08-28 16:43:34 +0800

    Drawer 引入问题修改

--- app/src/main/java/com/xuxi/coolweather/WeatherActivity.java
+++ app/src/main/java/com/xuxi/coolweather/WeatherActivity.java
@@ -138,6 +138,21 @@ public class WeatherActivity extends AppCompatActivity {
             }
         });
 
+//        replaceFragment(new ChooseAreaFragment());
+
+    }
+    public void replaceFragment(Fragment fragment){
+        //动态添加碎片
+        //创建待添加的碎片
+        //获取FragmentManager,在活动中可以直接调用getSupportFragemtnManager方法
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        //开始一个事务，通过调用beginTransaction
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        //向容器内添加或替换碎片，一般使用replace方法实现，需要传入容器的id和待添加的碎片实例
+//        transaction.replace(R.id.chooseww,fragment);
+//        transaction.addToBackStack(null);
+        //提交事务，调用commit方法来完成。
+        transaction.commit();
     }
 
 
